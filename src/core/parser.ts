55d2284e971382dc161ed69ec36e0bb8c35346c8
s3xysteak <86149525+s3xysteak@users.noreply.github.com>
2024-08-21 21:10:20 +0800

    refactor: parser

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,15 +1,8 @@
 import { basename, extname, join, resolve } from 'pathe'
 import { glob } from 'tinyglobby'
 import fs from 'fs-extra'
-import type { IconifyMeta, IconsData } from './types'
-import { toArray } from './utils'
-
-/**
- * 1. width
- * 2. height
- * 3. body
- */
-export const SVG_REGEX = /<svg[^>]*?viewBox="\s*\d+\s+\d+\s+(\d+)\s+(\d+)\s*"[^>]*>(.*?)<\/svg>/s
+import type { IconifyIcon, IconifyJSONIconsData, IconifyMeta, IconsData } from './types'
+import { notNullish, objectMap, toArray } from './utils'
 
 export async function normalizeIcons<T extends IconsData = IconsData>(options: T[], base: string) {
   /** key is prefix */
@@ -26,40 +19,68 @@ export async function normalizeIcons<T extends IconsData = IconsData>(options: T
     if (!prefix || !icons)
       return
 
-    const iconsMap = new Map<string, IconifyMeta['icons'][string]>()
-
     const paths = await glob(toArray(icons).map(p => join(p, '*.svg')), { cwd: base })
       .then(relatives => relatives.map(relative => resolve(base, relative)))
 
-    await Promise.all(paths.map(async (path) => {
-      const name = path.split('/').at(-1)
-      if (!name)
-        return
+    const _icons = Object.fromEntries(
+      await Promise.all(
+        paths.map(async (path) => {
+          const name = path.split('/').at(-1)
+          if (!name)
+            return
 
-      const svg = await fs.readFile(path, 'utf-8')
+          const svg = await fs.readFile(path, 'utf-8')
 
-      const match = svg.match(SVG_REGEX)
-      if (!match)
-        return
+          const val = parseIcon(svg)
+          if (!val)
+            return
 
-      iconsMap.set(
-        basename(name, extname(name)),
-        {
-          width: match[1],
-          height: match[2],
-          body: match[3].trim(),
-        },
-      )
-    }))
+          return [
+            basename(name, extname(name)),
+            val,
+          ] as const
+        }),
+      ).then(v => v.filter(notNullish)),
+    )
 
     map.set(prefix, {
       meta: {
         prefix,
-        icons: Object.fromEntries(iconsMap.entries()),
+        icons: _icons,
       },
       info: option,
     })
   }))
 
   return map
+}
+
+/**
+ * 1. width
+ * 2. height
+ * 3. body
+ */
+export const SVG_REGEX = /<svg[^>]*?viewBox="\s*\d+\s+\d+\s+(\d+)\s+(\d+)\s*"[^>]*>(.*?)<\/svg>/s
+
+export function parseIcon(svg: string): IconifyIcon | null {
+  const match = svg.match(SVG_REGEX)
+
+  const width = Number(match?.[1])
+  const height = Number(match?.[2])
+
+  return !!match && !Number.isNaN(width) && !Number.isNaN(height)
+    ? {
+        width,
+        height,
+        body: match[3].trim(),
+      }
+    : null
+}
+
+export function parseIcons(icons: Record<string, string>): IconifyJSONIconsData['icons'] {
+  return objectMap(icons, (k, v) => {
+    const val = parseIcon(v)
+
+    return val ? [k, val] : undefined
+  })
 }
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -2,3 +2,15 @@ export function toArray<T>(array?: T | T[]): T[] {
   array = array ?? []
   return Array.isArray(array) ? array : [array]
 }
+
+export function objectMap<K extends string, V, NK extends PropertyKey = K, NV = V>(obj: Record<K, V>, fn: (key: K, value: V) => [NK, NV] | undefined): Record<NK, NV> {
+  return Object.fromEntries(
+    Object.entries(obj)
+      .map(([k, v]) => fn(k as K, v as V))
+      .filter(notNullish),
+  ) as Record<NK, NV>
+}
+
+export function notNullish<T>(v: T | null | undefined): v is NonNullable<T> {
+  return v != null
+}
