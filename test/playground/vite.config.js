249abf55051c051dc99e7b58a967a82c177d0886
s3xysteak <s3xysteak@outlook.com>
2024-10-15 19:58:55 +0800

    test: chore

--- test/playground/vite.config.js
+++ test/playground/vite.config.js
@@ -16,14 +16,8 @@ export default defineConfig({
         hi: './test/playground/src/icons',
       },
       cwd: fileURLToPath(new URL('../../', import.meta.url)),
-      iconifyIntelliSense: true,
     }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   server: {
     host: '0.0.0.0',
   },
